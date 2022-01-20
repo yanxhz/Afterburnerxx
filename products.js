8fe981209e97fe5e0c48150c1fec5cf2fe94353e
yanxhz <96203290+yanxhz@users.noreply.github.com>
2022-01-20 10:44:42 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: 0,
+    name: "Nike Air Max 90 Premium",
+    price: 2099000,
+    instock: 100,
+    description:
+      "Sepatu Sneakers",
+    imgSrc: "./img/1.png",
+  },
+  {
+    id: 1,
+    name: "Nike Blazer Mid '77 Suede",
+    price: 2499000,
+    instock: 43,
+    description:
+      "Sepatu Sneakers",
+    imgSrc: "./img/2.png",
+  },
+  {
+    id: 2,
+    name: "Nike Mercurial Superfly 7",
+    price: 1999000,
+    instock: 10,
+    description:
+      "Sepatu Sneakers",
+    imgSrc: "./img/3.png",
+  },
+  {
+    id: 3,
+    name: "Nike Air Zoom Winflo 7",
+    price: 2599000,
+    instock: 5,
+    description:
+      "Sepatu Sneakers",
+    imgSrc: "./img/4.png",
+  },
+  {
+    id: 4,
+    name: "Nike PG 4 EP",
+    price: 2999000,
+    instock: 4,
+    description:
+      "Sepatu Sneakers",
+    imgSrc: "./img/5.png",
+  },
+  {
+    id: 5,
+    name: "Nike Benassi JD",
+    price: 3999000,
+    instock: 40,
+    description:
+      "Sandal",
+    imgSrc: "./img/6.png",
+  },
+];
